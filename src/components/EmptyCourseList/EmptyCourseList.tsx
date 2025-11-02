b0f0cac9ef31af5ed79e7badc07ffba5105ad232
Andrii Henyk <education.andriy@gmail.com>
2025-11-01 21:12:40 -0600

    Resolve test failures 3rd try

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -7,7 +7,7 @@ import './EmptyCourseList.scss'
 const EmptyCourseList: React.FC = () => {
     return (
         <div className="emptyCourseList">
-            <h3 className="emptyListTitle">Your List Is Empty</h3>
+            <h3 className="emptyListTitle">Course List is Empty</h3>
             <p className="emptyListText">Please use "Add New Course" button to add your first course</p>
             <Button buttonText={BUTTON_ADD_NEW_COURSE_TEXT} width='233px' height='50px' />
         </div>
