2db02f58a261e43f4041e8fb9113c451d0212b2f
Andrii Henyk <education.andriy@gmail.com>
2025-11-14 17:18:36 -0700

    Add <Link> to logo

--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -1,8 +1,9 @@
 import logo from '../../../../assets/logo.jpg';
+import { Link } from 'react-router-dom';
 
 import './Logo.scss'
 
 const Logo: React.FC = () => {
-    return (<img src={logo} alt="Logo" className='logo' />);
+    return (<Link to=''><img src={logo} alt="Logo" className='logo' /></Link>);
 }
 export default Logo;
