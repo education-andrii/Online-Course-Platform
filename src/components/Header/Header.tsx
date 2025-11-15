74cbce4de1076b770ba012c8aea85b16534db97b
Andrii Henyk <education.andriy@gmail.com>
2025-11-14 19:35:45 -0700

    Add UserAuth component to organize code and to conditionally render auth controls

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,20 @@
-import Logo from './components/Logo/Logo';
-import Button from '../../common/Button/Button';
+import { useLocation } from 'react-router-dom';
 
-import { BUTTON_LOGOUT_TEXT } from '../../constants';
+import Logo from './components/Logo/Logo';
+import UserAuth from './UserAuth/UserAuth';
 
 import './Header.scss'
 
-
 const Header: React.FC = () => {
+
+    const location = useLocation();
+
+    let isLogged = false;
+    let displayName = "John Doe";
+
     return (<header className='header'>
         <Logo></Logo>
-        <Button buttonText={BUTTON_LOGOUT_TEXT}></Button>
+        {(location.pathname !== '/login' && location.pathname !== '/registration') && <UserAuth isLogged={isLogged} displayName={displayName} />}
     </header>)
 }
 export default Header;
--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -4,6 +4,6 @@ import { Link } from 'react-router-dom';
 import './Logo.scss'
 
 const Logo: React.FC = () => {
-    return (<Link to=''><img src={logo} alt="Logo" className='logo' /></Link>);
+    return (<Link to='/'><img src={logo} alt="Logo" className='logo' /></Link>);
 }
 export default Logo;
