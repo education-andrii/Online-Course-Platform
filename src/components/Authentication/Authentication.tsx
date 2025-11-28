cb0919d7f0b9d872b14d5c1b46cac25c908f200c
Andrii Henyk <education.andriy@gmail.com>
2025-11-27 22:08:45 -0700

    Change submit handler function name

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -9,19 +9,19 @@ interface Props {
     title: string;
     children: ReactNode;
     formRef?: React.Ref<HTMLFormElement>;
-    handleSubmit: React.FormEventHandler<HTMLFormElement>;
+    onSubmit: React.FormEventHandler<HTMLFormElement>;
     buttonText?: string;
     linkPath: string;
     linkText: string;
     linkBoldText: string;
 }
 
-const Authentication: React.FC<Props> = ({ title, children, formRef, handleSubmit, buttonText, linkPath, linkText, linkBoldText }) => {
+const Authentication: React.FC<Props> = ({ title, children, formRef, onSubmit, buttonText, linkPath, linkText, linkBoldText }) => {
 
     return (
         <div className={styles.authenticationContainerWrapper}>
             <h1>{title}</h1>
-            <form ref={formRef} className={styles.authenticationForm} onSubmit={handleSubmit}>
+            <form ref={formRef} className={styles.authenticationForm} onSubmit={onSubmit}>
                 {children}
                 <Button buttonText={buttonText} type="submit" width='286px' height='50px' />
                 <Link to={linkPath}><span>{linkText} <strong>{linkBoldText}</strong></span></Link>
