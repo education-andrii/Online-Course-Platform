600e428e55a597af4d0831a3f10b428f1470f49e
Andrii Henyk <education.andriy@gmail.com>
2025-11-29 20:38:59 -0700

    Add logic to display Name. Remove UserAuth component, it is no longer needed. Add Logout functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,50 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import Logo from './components/Logo/Logo';
-import UserAuth from './UserAuth/UserAuth';
+
+import Button from '@/common/Button/Button';
+
+import { BUTTON_LOGOUT_TEXT } from '../../constants';
 
 import './Header.scss'
-
-const Header: React.FC = () => {
-
+import { useEffect, useState } from 'react';
+interface Props {
+    isAuthorized: boolean;
+    onLogOut: Function
+}
+const Header: React.FC<Props> = ({ isAuthorized, onLogOut }) => {
+    const navigate = useNavigate()
     const location = useLocation();
+    const [displayName, setDisplayName] = useState<string>('')
+    let isLogged = isAuthorized
 
-    let isLogged = false;
-    let displayName = "John Doe";
+    useEffect(() => {
+        if (isAuthorized) {
+            const getUserName = () => {
+                const name = localStorage.getItem('user') || '';
+                setDisplayName(name)
+            }
+            getUserName();
+        } else {
+            setDisplayName('')
+        }
+    }, [isAuthorized])
+
+    const handleLogOutClick = () => {
+        onLogOut();
+        navigate('/login');
+    }
 
     return (<header className='header'>
         <Logo></Logo>
-        {(location.pathname !== '/login' && location.pathname !== '/registration') && <UserAuth isLogged={isLogged} displayName={displayName} />}
+        {(location.pathname !== '/login' && location.pathname !== '/registration') &&
+            (isLogged &&
+                <div>
+                    <span className='headerDisplayName'>{displayName}</span>
+                    <Button buttonText={BUTTON_LOGOUT_TEXT} onClick={handleLogOutClick}></Button>
+                </div>
+            )
+        }
     </header>)
 }
 export default Header;
