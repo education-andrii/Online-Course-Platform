55126b5bc10fd522c2af12c39ab192b95d1f3b0e
Andrii Henyk <education.andriy@gmail.com>
2025-12-03 21:45:06 -0700

    Add children props. Remove p tag and refactor styling

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,6 +1,7 @@
 import './Button.scss'
 
 interface ButtonProps {
+    children?: React.ReactNode
     className?: string;
     id?: string;
     buttonText?: React.ReactNode;
@@ -12,10 +13,11 @@ interface ButtonProps {
     height?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ className, id, buttonText, onClick, type = "button", role, name, width = '180px', height = '50px' }) => {
+const Button: React.FC<ButtonProps> = ({ children, className, id, buttonText, onClick, type = "button", role, name, width = '180px', height = '50px' }) => {
     return (
         <button className={`button ${className ?? ""}`} id={id} onClick={onClick} type={type} role={role} name={name} style={{ width, height }}>
-            <p className='buttonText'>{buttonText}</p>
+            {buttonText}
+            {children}
         </button>
     )
 }
