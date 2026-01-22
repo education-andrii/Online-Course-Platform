e8a30969b5340b1bcb366b3344beec6b7743888d
Andrii Henyk <education.andriy@gmail.com>
2026-01-22 16:43:29 -0700

    build: setup dynamic port for backend

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   app.enableCors();
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  // await app.listen(4000);
+await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
